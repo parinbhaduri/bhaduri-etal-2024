930fdb98ea876457c0deb1b3a7c32c222d35fde8
parinbhaduri <pbb62@cornell.edu>
2024-06-14 11:46:52 -0400

    Updated toy model figures
    Create file to recreate figure 2. Updated RA curves

--- workflow/toy_model/RA_curves.jl
+++ workflow/toy_model/RA_curves.jl
@@ -7,11 +7,11 @@ Pkg.instantiate()
 using Plots
 using Plots.PlotMeasures
 #Create function
-function move_curve(x; ra = 0.3, scale = 1, base_prob = 0.025)
+function move_curve(x; ra = 0.3, scale = 0, base_prob = 0.025)
     if x == 0.0
         prob = base_prob
     else
-        init_prob = 1/(1+ exp(-10((scale * x) - ra))) + base_prob
+        init_prob = 1/(1+ exp(-(x - ra)/(0.1-scale))) + base_prob
         prob = init_prob > 1 ? 1 : init_prob
     end
     return prob
@@ -24,9 +24,9 @@ y1 = move_curve.(x; ra = 0.3)
 y2 = move_curve.(x; ra = 0.7)
 
 #Fixed Effect Curves
-y1_scale = move_curve.(x; ra = 0.3, scale = 0.0)
-y2_scale = move_curve.(x; ra = 0.3, scale = 0.5)
-y3_scale = move_curve.(x; ra = 0.3, scale = 0.7)
+y1_scale = move_curve.(x; ra = 0.3, scale = 0.03)
+y2_scale = move_curve.(x; ra = 0.3, scale = 0.05)
+y3_scale = move_curve.(x; ra = 0.3, scale = 0.07)
 
 log_fig = Plots.plot(x.*10,[y1 y y2], label = ["High Risk Aversion" "Medium Risk Aversion" "Low Risk Aversion"], lw = 3,
 xticks = ([0,1,3,5,7,10], string.([0,1,3,5,7,10])), legend = :outerbottom, legendcolumns = 3, legend_foreground_color = :transparent, dpi = 300)
@@ -35,7 +35,7 @@ Plots.ylabel!("Movement Probability")
 
 savefig(log_fig, joinpath(pwd(),"figures/log_func.png"))
 
-log_scale_fig = Plots.plot(x.*10,[y1 y1_scale y2_scale y3_scale], label = ["fixed effect = 0" " fixed effect = 0.3" "fixed effecte = 0.5" "fixed effect = 0.7"], lw = 3,
+log_scale_fig = Plots.plot(x.*10,[y1 y1_scale y2_scale y3_scale], label = ["fixed effect = 0" " fixed effect = 0.03" "fixed effecte = 0.05" "fixed effect = 0.07"], lw = 3,
 xticks = ([0,1,3,5,7,10], string.([0,1,3,5,7,10])), legend = :outerbottom, legendcolumns = 2, legend_foreground_color = :transparent, dpi = 300)
 Plots.xlabel!("Flood Events per Decade")
 Plots.ylabel!("Movement Probability")
