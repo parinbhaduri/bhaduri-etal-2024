b9e5b68feb288c2c9558c1d3507621a0f83a588c
parinbhaduri <pbb62@cornell.edu>
2024-10-19 16:23:09 -0400

    Updated Figure Scripts
    Updated Scripts to plot abm evolution and risk transference for both experiments on the same graph

--- scripts/plot_abm_evolution.jl
+++ scripts/plot_abm_evolution.jl
@@ -10,7 +10,18 @@ using ColorSchemes
 #import GEV functions from toy model
 include(joinpath(dirname(@__DIR__), "workflow/toy_model/src/toy_ABM_functions.jl")) #for GEV_return function
 
-#Read in ABM ensemble evolution data
+### Read in ABM ensemble evolution data
+##Baltimore Experiment
+adf = DataFrame(CSV.File(joinpath(dirname(@__DIR__),"workflow/CHANCE_C/dataframes/adf_balt_city.csv")))
+
+balt_base_high = subset(adf, :levee => ByRow(isequal(false)), :slr_scen => ByRow(isequal("medium")), :risk_averse => ByRow(isequal(0.3)))
+balt_base_low = subset(adf, :levee => ByRow(isequal(false)), :slr_scen => ByRow(isequal("medium")),  :risk_averse => ByRow(isequal(0.7)))
+
+balt_levee_high = subset(adf, :levee => ByRow(isequal(true)), :slr_scen => ByRow(isequal("medium")), :risk_averse => ByRow(isequal(0.3)))
+balt_levee_low = subset(adf, :levee => ByRow(isequal(true)), :slr_scen => ByRow(isequal("medium")), :risk_averse => ByRow(isequal(0.7)))
+
+
+##Idealized Experiment
 adf_base = DataFrame(CSV.File(joinpath(dirname(@__DIR__), "workflow/toy_model/dataframes/adf_base.csv")))
 #Separate high RA and low RA
 adf_base_high = filter(:risk_averse => isequal(0.3), adf_base)
@@ -75,17 +86,25 @@ pop_change = [(max_response.count_floodplain_fam[i] .- max_response.count_floodp
 """
 
 ##Plot Baseline Results
-fig = Figure(size = (1000, 1000), fontsize = 18, pt_per_unit = 1, figure_padding = 20)
+fig = Figure(size = (1000, 1000), fontsize = 16, pt_per_unit = 1, figure_padding = 20)
 ga = fig[1, 1:2] = GridLayout()
 gb = fig[2, 1:2] = GridLayout()
+gc = fig[3, 1:2] = GridLayout()
 
-ax1 = Axis(ga[1, 1], ylabel = "Change in Population (count)", xlabel = "Time Since Major Flood (years)", title = " a. Floodplain Population Response after Major Flood Event in No Levee Scenario",
+ax1 = Axis(ga[1, 1], ylabel = rich("Change in Population (count)"; font = :bold), xlabel = rich("Time Since Major Flood (years)"; font = :bold),
+title = " a. Floodplain Population Response after Major Flood Event in No Levee Scenario (Idealized)",
 limits = ((0,13), nothing), xgridvisible = false)
 hidespines!(ax1, :t, :r)
 
-ax2 = Axis(gb[1, 1], ylabel = "Difference in Population (count)", xlabel = "Model Timestep (year)", title = "b. Difference in Floodplain Population between Levee and No Levee Scenario", 
+ax2 = Axis(gb[1, 1], ylabel = rich("Difference in Population (count)"; font = :bold), xlabel = rich("Model Timestep (year)"; font = :bold),
+title = "b. Difference in Floodplain Population between Levee and No Levee Scenario (Idealized)", titlesize = 18, 
 limits = ((0,50), (nothing, 400)), xgridvisible = false)
 hidespines!(ax2, :t, :r)
+
+ax3 = Axis(gc[1, 1], ylabel = rich("Difference in Population (count)"; font = :bold), xlabel = rich("Model Timestep (year)"; font = :bold),
+title = "c. Difference in Floodplain Population between Levee and No Levee Scenario (Baltimore)", titlesize = 18, 
+ xgridvisible = false, yticks = ([-5e3, 0, 5e3, 1e4], ["-5000","0","5000","10000"]), limits = ((0,50), (nothing, nothing)))
+hidespines!(ax3, :t, :r)
 
 
 palette = ColorSchemes.okabe_ito
@@ -117,8 +136,8 @@ axislegend(ax1, [elem_1, elem_2] , ["High Risk Aversion", "Low Risk Aversion"], 
 pop_diff_high = transpose(reshape(adf_levee_high.count_floodplain_fam, (51,1000))) .-  transpose(reshape(adf_base_high.count_floodplain_fam, (51,1000)))
 pop_diff_low = transpose(reshape(adf_levee_low.count_floodplain_fam, (51,1000))) .- transpose(reshape(adf_base_low.count_floodplain_fam, (51,1000)))
 
-CairoMakie.series!(ax2, pop_diff_high, solid_color = (palette[1], 0.25), linewidth = 1, overdraw = true, transparency = true)
-CairoMakie.series!(ax2, pop_diff_low, solid_color = (palette[2], 0.25), linewidth = 1, overdraw = true, transparency = true)
+CairoMakie.series!(ax2, pop_diff_high, solid_color = (palette[1], 0.25), linewidth = 1)#, overdraw = true, transparency = true)
+CairoMakie.series!(ax2, pop_diff_low, solid_color = (palette[2], 0.25), linewidth = 1)#, overdraw = true, transparency = true)
 #Create Legend
 elem_1 = [LineElement(color = palette[1], linestyle = nothing)]
 
@@ -127,6 +146,21 @@ elem_2 = [LineElement(color = palette[2], linestyle = nothing)]
 axislegend(ax2, [elem_1, elem_2] , ["High Risk Aversion", "Low Risk Aversion"], position = :lt, orientation = :horizontal,
  framevisible = false)
 
+
+#Plot Difference in floodplain population between levee and no levee scenario
+balt_diff_high = transpose(reshape(balt_levee_high.sum_population_f_c_bgs, (51,1000))) .-  transpose(reshape(balt_base_high.sum_population_f_c_bgs, (51,1000)))
+balt_diff_low = transpose(reshape(balt_levee_low.sum_population_f_c_bgs, (51,1000))) .- transpose(reshape(balt_base_low.sum_population_f_c_bgs, (51,1000)))
+
+CairoMakie.series!(ax3, balt_diff_high, solid_color = (palette[1], 0.25), linewidth = 1)#, overdraw = true, transparency = true)
+CairoMakie.series!(ax3, balt_diff_low, solid_color = (palette[2], 0.25), linewidth = 1)#, overdraw = true, transparency = true)
+#Create Legend
+elem_1 = [LineElement(color = palette[1], linestyle = nothing)]
+
+elem_2 = [LineElement(color = palette[2], linestyle = nothing)]
+
+axislegend(ax3, [elem_1, elem_2] , ["High Risk Aversion", "Low Risk Aversion"], position = :lt, orientation = :horizontal,
+ framevisible = false)
+
 display(fig)
 
-CairoMakie.save(joinpath(pwd(),"figures/abm_evolution.png"), fig)
+CairoMakie.save(joinpath(pwd(),"figures/abm_response.png"), fig)
--- scripts/plot_risk_shift.jl
+++ scripts/plot_risk_shift.jl
@@ -9,8 +9,9 @@ using CairoMakie
 using ColorSchemes
 using FileIO
 
-## Read in dataframe for Idealized Experiment
+## Read in dataframes for Idealized Experiment
 occ_med = DataFrame(CSV.File(joinpath(dirname(@__DIR__),"workflow/toy_model/dataframes/breach_base.csv")))
+occ_med_RA_low = DataFrame(CSV.File(joinpath(dirname(@__DIR__),"workflow/toy_model/dataframes/breach_base_RA_low.csv")))
 
 
 ## Read in dataframes for Baltimore Experiment. Calculate Risk Shifting 
@@ -52,50 +53,46 @@ thresh_balt = zeros(length(event_size))
 
 ## Plot results
 #Create backdrop
-fig = Figure(size = (1500,600), fontsize = 18, pt_per_unit = 1, figure_padding = 18)
+fig = Figure(size = (700,800), fontsize = 16, pt_per_unit = 1, figure_padding = 18)
 
-ax1 = Axis(fig[1, 1:2], ylabel = rich("Difference in Occupied Exposure";font=:bold), xlabel = rich("Return Period (years)";font=:bold), xscale = log10,
- xticks = ([10,100,1000], string.([10,100,1000])), limits = ((10,1000), nothing), xgridvisible = false, titlealign = :center, title = "Comparing Flood Impact between Levee and No Levee Scenario")
+ax1 = Axis(fig[1, 1:2], ylabel = rich("Difference in\n Occupied Exposure";font=:bold), xlabel = rich("Return Period (years)";font=:bold), xscale = log10,
+titlesize = 18, xticks = ([10,100,1000], string.([10,100,1000])), limits = ((10,1000), nothing), xgridvisible = false, titlealign = :left, title = "a. Idealized Experiment")
 
-ax2 = Axis(fig[2, 1:2], ylabel = rich("Difference in Loss (\$ Thousands)\n";font=:bold),
- xlabel = rich("Surge Event (m)";font=:bold), yticks = ([-2e4, 0, 2e4, 4e4], ["-20","0","20","40"]), limits = ((0.75,4), nothing),
- xgridvisible = false, titlealign = :center,
-  title = "Comparing Flood Impact between Levee and No Levee Scenario")
+ax2 = Axis(fig[2, 1:2], ylabel = rich("Difference in Loss\n (\$ Thousands)";font=:bold),
+ xlabel = rich("Surge Event (m)";font=:bold), titlesize = 18, yticks = ([-2e4, 0, 2e4, 4e4], ["-20","0","20","40"]), limits = ((0.75,4), nothing),
+ xgridvisible = false, titlealign = :left, title = "b. Baltimore Experiment")
 
 
 Palette = ColorSchemes.okabe_ito
-#Panel A
-CairoMakie.lines!(ax1, ret_per, occ_med.median, color = Palette[1], linewidth = 2.5)
+##Panel A
+CairoMakie.lines!(ax1, ret_per, occ_med.median, color = Palette[1], linewidth = 3.5)
 #, label = false)
-
 CairoMakie.band!(ax1, ret_per, occ_med.LB, occ_med.RB, color = (Palette[1], 0.35))
 
+CairoMakie.lines!(ax1, ret_per, occ_med_RA_low.median, color = Palette[2], linewidth = 3.5)
+#, label = false)
+CairoMakie.band!(ax1, ret_per, occ_med_RA_low.LB, occ_med_RA_low.RB, color = (Palette[2], 0.35))
+
 CairoMakie.lines!(ax1, ret_per, thresh_ideal, linestyle = :dash, color = "black", linewidth = 2)
+#Add Floodwall Height
+CairoMakie.vlines!(ax1, 100, color = Palette[3], linewidth = 2.5)
 
 
-
-
-
-
-
-
-
-CairoMakie.lines!(ax2, event_size, diff_med_low, color = Palette[2], linewidth = 5)
+##Panel B
+CairoMakie.lines!(ax2, event_size, diff_med_low, color = Palette[2], linewidth = 3.5)
  #, label = false)
- 
 CairoMakie.band!(ax2, event_size, diff_LB_low, diff_UB_low, color = (Palette[2], 0.35))
 
 
-CairoMakie.lines!(ax2, event_size, diff_med, color = Palette[1], linewidth = 5)
+CairoMakie.lines!(ax2, event_size, diff_med, color = Palette[1], linewidth = 3.5)
  #, label = false)
- 
 CairoMakie.band!(ax2, event_size, diff_LB, diff_UB, color = (Palette[1], 0.35))
  
 CairoMakie.lines!(ax2, event_size, thresh_balt, linestyle = :dash, color = "black", linewidth = 2)
 
 
 #Add 100-year event and Floodwall height
-CairoMakie.vlines!(ax2, [1.98, 2.804], color = [Palette[3], Palette[7]], linewidth = 2.5)
+CairoMakie.vlines!(ax2, 2.804, color = Palette[3], linewidth = 2.5) # 100-yr event = 1.98 m
 
 #Create Legend
 elem_1 = [LineElement(color = Palette[1], linestyle = :solid, linewidth = 5), PolyElement(color = (Palette[1], 0.35))]
@@ -104,12 +101,12 @@ elem_2 = [LineElement(color = Palette[2], linestyle = :solid, linewidth = 5), Po
 
 elem_3 = [LineElement(color = Palette[3], linestyle = :solid)]
 
-elem_4 = [LineElement(color = Palette[7], linestyle = :solid)]
+#elem_4 = [LineElement(color = Palette[7], linestyle = :solid)]
 
-axislegend(ax2, [[elem_1, elem_2], [elem_3, elem_4]] , [["High Risk Aversion", "Low Risk Aversion"],
-["100-Year Event", "Flood Wall Height"]], ["Ensemble Summary", "Design Levels"], 
-position = :lt, orientation = :vertical, framevisible = false, labelsize = 18)
+Legend(fig[3, 1:2], [elem_1, elem_2, elem_3] , ["High Risk Aversion", "Low Risk Aversion", "Levee Height"],
+orientation = :horizontal, framevisible = false, labelsize = 16) #position = :lt,
 
+rowgap!(fig.layout, 2, 25)
 display(fig)
 
-CairoMakie.save(joinpath(pwd(),"figures/balt_rs_pres.svg"), fig)
+CairoMakie.save(joinpath(pwd(),"figures/risk_transference.png"), fig)
